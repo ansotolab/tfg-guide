8ddbfc4115a9e32ddfc5d80cc1d4d4e701895154
Antonio Soto <ansotolab@gmail.com>
2021-05-31 12:52:35 +0200

    Modificando para ejecución de pruebas.

--- customers-service/src/test/java/es/ansotolab/customersservice/CustomersServiceApplicationTests.java
+++ customers-service/src/test/java/es/ansotolab/customersservice/CustomersServiceApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
-@SpringBootTest
+//@SpringBootTest
 class CustomersServiceApplicationTests {
 
 	@Test
